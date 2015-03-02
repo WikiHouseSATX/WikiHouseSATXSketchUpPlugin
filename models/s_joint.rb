d5f83d8a8640c03ee58402e8505c7af7033c00cc
Dirk Elmendorf <dirk@r26d.com>
2015-03-02 16:13:33 -0600

    First working s joint !
    I haven't used trig since high school. Lot to remember - who am I
    kidding we googled a lot of tstuff.

--- models/s_joint.rb
+++ models/s_joint.rb
@@ -1,44 +1,59 @@
 class SJoint < Joint
-	# Legth Joint is Wood T1
-	#                          Wood T2 B4 Wood
-	#                          Wood  B3 B2 B1
-	# Right sjoint is B1 B2 B3 T1Wood
-	#                           Wood B4                   Wood
-	#                           T2 Wood
-	# I measured the model in sketchup and use those measurements to make
-	# the ratios
-	# total_HEIGHT = 22 13/16  22.8125
-	#          Width X height
-	# b1 = 7 11/16 X  7 5/8  7.6875 x  7.625
+	# This class was based on the original S-Joint 3.4
+	#  We made some changes
+	# The parallelogram  had 77.5 and 102.5 as the angles - we changed it to 80/100 degrees
+	# The width of the parallelogram  was slightly less than the  width of the baord - we made it the width of the baord
 
-	# b2 = 3 3/4 X 7 5/8  3.75 x 7.625
 
-	# b3 = 8 x 7 5/8  =  8 x 7.625
+	def self.version
+		3.5
+	end
 
-	# b4 = 8 x 7 11/16  8 x 7.625
-
-	# triangle1 = 3 1/2 x 15 5/16  3.5 x 15.3125
-	MEASURED_HEIGHT = 22.8125  unless defined? MEASURED_HEIGHT
-	MEASURED_B1_WIDTH = 7.6875  unless defined? MEASURED_B1_WIDTH
-	MEASURED_B1_HEIGHT = 7.625  unless defined? MEASURED_B1_HEIGHT
-	MEASURED_B2_WIDTH = 3.75 unless defined? MEASURED_B2_WIDTH
-	MEASURED_B3_WIDTH = 8 unless defined? MEASURED_B3_WIDTH
-	MEASURED_B4_HEIGHT = 7.625 unless defined? MEASURED_B4_HEIGHT
-	MEASURED_T1_WIDTH = 3.5 unless defined? MEASURED_T1_WIDTH
-	MEASURED_T1_HEIGHT = 15.3125 unless defined? MEASURED_T1_HEIGHT
 	def total_height
 		@total_height
 	end
 	def set_total_height_from_line(start, finish, orientation)
 		index = orientation == :y ? 0 : 1
 
-		@total_height = finish[index] - start[index]
+		@total_height = (finish[index] - start[index]).abs
 	end
 	def size_ratio(measurement)
 		ratio = measurement.to_f/MEASURED_HEIGHT
 		total_height.to_f * ratio
 	end
+	def self.degrees_to_radians(degrees)
+		degrees * Math::PI / 180
+	end
+	def build_parallelogram_points(x1,y1, b_length, h_length, x1_angle_in_degrees)
+		c1 = [x1, y1,0]
+		c2 = [x1 + b_length, y1,0]
+		bottom_triangle_leg = h_length/Math.tan(SJoint.degrees_to_radians(x1_angle_in_degrees))
+		puts "#{x1} #{b_length} #{h_length} #{bottom_triangle_leg}"
+		if x1_angle_in_degrees > 90
+			c3 = [x1 + b_length + - bottom_triangle_leg, y1 - h_length,0]
 
+		else
+			c3 = [x1 + b_length + bottom_triangle_leg, y1 - h_length,0]
+		end
+
+
+		c4 = [c3[0]  - b_length, c3[1],0]
+		[c1, c2, c3, c4]
+	end
+  def draw_parallelogram(x1,y1, b_length, h_length, x1_angle_in_degrees, omit_sides = [])
+
+	  points = build_parallelogram_points(x1,y1, b_length, h_length, x1_angle_in_degrees)
+		c1 = points[0]
+		c2 = points[1]
+		c3 = points[2]
+		c4 = points[3]
+		edges = []
+		edges << Sketchup.active_model.entities.add_line(c1, c2) unless omit_sides.include? :s1
+		edges << Sketchup.active_model.entities.add_line(c2, c3) unless omit_sides.include? :s2
+   	edges << Sketchup.active_model.entities.add_line(c3, c4) unless omit_sides.include? :s3
+		edges << Sketchup.active_model.entities.add_line(c4,c1) unless omit_sides.include? :s4
+		edges
+	end
 	def draw_triangle(x1,y1, x2, y2, x3, y3, omit_sides = [])
 		p1 = [x1, y1, 0]
 		p2 = [x2, y2, 0]
@@ -63,45 +78,69 @@ class SJoint < Joint
 		edges << Sketchup.active_model.entities.add_line(c4,c1) unless omit_sides.include? :s4
 		edges
 	end
-	def join!(start, finish, orientation)
+	def join!(first_point, last_point, orientation)
+
+		if first_point[1] < last_point[1]
+			start = last_point
+			finish = first_point
+		else
+			start = first_point
+			finish = last_point
+		end
 		#start & finish is a straight line down the middle of the part.
 		#need to add kind of joint
 		puts "I'm an SJoint" if WikiHouse::LOG_ON
-		Sketchup.active_model.entities.add_line(start, finish)
+	#Mid point Line
+	#	Sketchup.active_model.entities.add_line(start, finish)
 		set_total_height_from_line(start, finish, orientation)
 		puts "Total Height #{total_height}"
-		b1_width = size_ratio(MEASURED_B1_WIDTH)
-		b1_height = size_ratio(MEASURED_B1_HEIGHT)
-		b2_width = size_ratio(MEASURED_B2_WIDTH)
-		b2_height = size_ratio(MEASURED_B1_HEIGHT)
-		b3_width = size_ratio(MEASURED_B3_WIDTH)
-		b3_height = size_ratio(MEASURED_B1_HEIGHT)
-		b4_width =  size_ratio(MEASURED_B3_WIDTH)
-		b4_height = size_ratio(MEASURED_B4_HEIGHT)
-		t1_width = size_ratio(MEASURED_T1_WIDTH)
-		t1_height = size_ratio(MEASURED_T1_HEIGHT)
-		t2_width = size_ratio(MEASURED_T1_WIDTH)
-		t2_height = size_ratio(MEASURED_T1_HEIGHT)
+		puts "Start #{start}"
+		puts "Finish #{finish}"
+		#71.45 degrees makes
+		angle_degrees = 71.45
+		one_third_height = total_height/3
+		# outer_parallelogram = draw_parallelogram(finish[0] - (2 * one_third_height),
+		#  start[1], total_height,
+		#  total_height,angle_degrees, [])
+	parallelogram_points = build_parallelogram_points(finish[0] - (2 * one_third_height),
+			start[1], total_height,
+			total_height,angle_degrees)
+    x1 = finish[0] - (2 * one_third_height)
+		y1 = finish[1] + (1 * one_third_height)
+		y2 = finish[1] + (2 * one_third_height)
+		x2 = finish[0] + (2 * one_third_height)
+	#	Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y1 ,0])
+	#	Sketchup.active_model.entities.add_line([x1, y2,0], [x2, y2,0])
 
-		# Draw the Shapes onto the part
-		# Right side
-		#b2
-		b2 = draw_box(finish[0],finish[1],
-		finish[0] - b2_width, finish[1] - b2_height,[:s1,:s2, :s3] )
-		b1 = draw_box( finish[0] - b2_width, finish[1],
-		finish[0] - b2_width - b1_width, finish[1] - b1_height ,[:s1,:s2, :s3] )
-		b3 = draw_box(finish[0] + b3_width ,finish[1],
-		finish[0] , finish[1] - b3_height ,[:s1,:s2, :s3, :s4] )
-		b4 = draw_box(finish[0] + b3_width ,finish[1] - b3_height,
-		finish[0] , finish[1] - b3_height - b4_height ,[:s1,:s2, :s3, :s4])
-		t1 = draw_triangle( finish[0] + b3_width ,finish[1],
-			finish[0] + b3_width, finish[1] - t1_height,
-			finish[0] + b3_width + t1_width, finish[1] - t1_height,[:s1,:s2, :s3]
-		)
-		t2 = draw_triangle( 	finish[0] - b2_width - b1_width, finish[1] - b1_height,
-			finish[0] - b2_width - b1_width, finish[1] - b1_height - t2_height,
-			finish[0] - b2_width - b1_width + t2_width, finish[1] - b1_height - t2_height,
-		[:s1, :s2, :s3]	)
+		bottom_triangle_leg = one_third_height/Math.tan(SJoint.degrees_to_radians(angle_degrees))
+		x1 = parallelogram_points[3][0]
+		y1 = parallelogram_points[3][1]
+		x2 = finish[0] - (2 * one_third_height) + bottom_triangle_leg
+		y2 = finish[1] + (2 * one_third_height)
+
+		Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y2 ,0])
+
+
+		x1 = finish[0] - (2 * one_third_height) + bottom_triangle_leg
+		x2 = finish[0]
+		y =  finish[1] + (2 * one_third_height)
+		Sketchup.active_model.entities.add_line([x1, y,0], [x2, y ,0])
+		x = finish[0]
+		y1 =  finish[1] + (1 * one_third_height)
+		y2 =  finish[1] + (2 * one_third_height)
+		Sketchup.active_model.entities.add_line([x, y1,0], [x, y2 ,0])
+		x1 = finish[0]
+		x2 = parallelogram_points[2][0] - bottom_triangle_leg
+		y =  finish[1] + (1 * one_third_height)
+		Sketchup.active_model.entities.add_line([x1, y,0], [x2, y ,0])
+
+		x1 = parallelogram_points[2][0] - bottom_triangle_leg
+		y1 = finish[1] + (1 * one_third_height)
+		x2 = parallelogram_points[1][0]
+		y2 = parallelogram_points[1][1]
+		Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y2 ,0])
+
+
 		# Left Side
 		# Fuse them into the part
 		# Turn into group
