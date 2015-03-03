145bf71e87b1fe1988394134626fff4237ce2e4c
Dirk Elmendorf <dirk@r26d.com>
2015-03-03 05:44:32 -0600

    Lock Gap
    Fixed it so that the lock is a deleted face gap

--- models/s_joint.rb
+++ models/s_joint.rb
@@ -40,17 +40,14 @@ class SJoint < Joint
 		c4 = [c3.x  - b_length, c3.y,0]
 		[c1, c2, c3, c4]
 	end
-  def draw_parallelogram(x1,y1, b_length, h_length, x1_angle_in_degrees, omit_sides = [])
+	def draw_parallelogram(x1,y1, b_length, h_length, x1_angle_in_degrees, omit_sides = [])
 
-	  points = build_parallelogram_points(x1,y1, b_length, h_length, x1_angle_in_degrees)
-		c1 = points[0]
-		c2 = points[1]
-		c3 = points[2]
-		c4 = points[3]
+		c1,c2,c3,c4 = build_parallelogram_points(x1,y1, b_length, h_length, x1_angle_in_degrees)
+
 		edges = []
 		edges << Sketchup.active_model.entities.add_line(c1, c2) unless omit_sides.include? :s1
 		edges << Sketchup.active_model.entities.add_line(c2, c3) unless omit_sides.include? :s2
-   	edges << Sketchup.active_model.entities.add_line(c3, c4) unless omit_sides.include? :s3
+		edges << Sketchup.active_model.entities.add_line(c3, c4) unless omit_sides.include? :s3
 		edges << Sketchup.active_model.entities.add_line(c4,c1) unless omit_sides.include? :s4
 		edges
 	end
@@ -65,12 +62,16 @@ class SJoint < Joint
 
 		edges
 	end
-	def draw_box(top_right_x, top_right_y, bottom_left_x, bottom_left_y, omit_sides = [])
-
+	def build_box(top_right_x, top_right_y, bottom_left_x, bottom_left_y)
 		c1 = [top_right_x,top_right_y,0]
 		c2 = [top_right_x,bottom_left_y,0]
 		c3 = [bottom_left_x, bottom_left_y,0]
 		c4 = [bottom_left_x,top_right_y,0]
+		[c1, c2, c3 ,c4]
+	end
+	def draw_box(top_right_x, top_right_y, bottom_left_x, bottom_left_y, omit_sides = [])
+
+		c1, c2, c3, c4 = build_box(top_right_x, top_right_y, bottom_left_x, bottom_left_y)
 		edges = []
 		edges << Sketchup.active_model.entities.add_line(c1, c2) unless omit_sides.include? :s1
 		edges << Sketchup.active_model.entities.add_line(c2, c3) unless omit_sides.include? :s2
@@ -90,8 +91,8 @@ class SJoint < Joint
 		#start & finish is a straight line down the middle of the part.
 		#need to add kind of joint
 		puts "I'm an SJoint" if WikiHouse::LOG_ON
-	#Mid point Line
-	#	Sketchup.active_model.entities.add_line(start, finish)
+		#Mid point Line
+		#	Sketchup.active_model.entities.add_line(start, finish)
 		set_total_height_from_line(start, finish, orientation)
 		puts "Total Height #{total_height}"
 		puts "Start #{start}"
@@ -102,15 +103,15 @@ class SJoint < Joint
 		# outer_parallelogram = draw_parallelogram(finish.x - (2 * one_third_height),
 		#  start.y, total_height,
 		#  total_height,angle_degrees, [])
-	parallelogram_points = build_parallelogram_points(finish.x - (2 * one_third_height),
+		parallelogram_points = build_parallelogram_points(finish.x - (2 * one_third_height),
 			start.y, total_height,
-			total_height,angle_degrees)
-    x1 = finish.x - (2 * one_third_height)
+		total_height,angle_degrees)
+		x1 = finish.x - (2 * one_third_height)
 		y1 = finish.y + (1 * one_third_height)
 		y2 = finish.y + (2 * one_third_height)
 		x2 = finish.x + (2 * one_third_height)
-	#	Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y1 ,0])
-	#	Sketchup.active_model.entities.add_line([x1, y2,0], [x2, y2,0])
+		#	Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y1 ,0])
+		#	Sketchup.active_model.entities.add_line([x1, y2,0], [x2, y2,0])
 
 		bottom_triangle_leg = one_third_height/Math.tan(SJoint.degrees_to_radians(angle_degrees))
 		x1 = parallelogram_points[3].x
@@ -125,10 +126,35 @@ class SJoint < Joint
 		x2 = finish.x
 		y =  finish.y + (2 * one_third_height)
 		Sketchup.active_model.entities.add_line([x1, y,0], [x2, y ,0])
+
 		x = finish.x
 		y1 =  finish.y + (1 * one_third_height)
 		y2 =  finish.y + (2 * one_third_height)
-		Sketchup.active_model.entities.add_line([x, y1,0], [x, y2 ,0])
+		#lock goes in here
+		# #lock
+		width_of_lock_slot_inches = total_height/2.0
+		thickness = @joiner.sheet.thickness
+		# #top Right
+		lx1 = finish.x + (width_of_lock_slot_inches/2.0)
+		ly1 = start.y - (total_height/2.0) + thickness/2.0
+		# #bottom left
+		lx2 = finish.x - (width_of_lock_slot_inches/2.0)
+		ly2 = ly1 - thickness
+		lc1, lc2, lc3, lc4 = build_box(lx1, ly1, lx2, ly2)
+		#Makes a cross
+		# Top
+		Sketchup.active_model.entities.add_line([x, y1,0], [x,ly2 ,0])
+		#lock
+
+		Sketchup.active_model.entities.add_line([x, lc1.y,0], lc1 )
+		Sketchup.active_model.entities.add_line(lc2,lc3)
+		Sketchup.active_model.entities.add_line(lc3,lc4)
+		Sketchup.active_model.entities.add_line(lc4, [x, lc4.y,0])
+		# lock left
+		#Bottom
+		Sketchup.active_model.entities.add_line([x, ly1,0], [x, y2 ,0])
+		lock_face = Sketchup.active_model.active_entities.add_face([lc1, lc2, lc3, lc4])
+		lock_face.erase!
 		x1 = finish.x
 		x2 = parallelogram_points[2].x - bottom_triangle_leg
 		y =  finish.y + (1 * one_third_height)
@@ -140,17 +166,7 @@ class SJoint < Joint
 		y2 = parallelogram_points[1].y
 		Sketchup.active_model.entities.add_line([x1, y1,0], [x2, y2 ,0])
 		#
-		# #lock
-		 width_of_lock_slot_inches = total_height/2.0
-		 thickness = @joiner.sheet.thickness
-		# #top Right
-		 x1 = finish.x + (width_of_lock_slot_inches/2.0)
-		 y1 = start.y - (total_height/2.0) + thickness/2.0
-		# #bottom left
-		 x2 = finish.x - (width_of_lock_slot_inches/2.0)
-		 y2 = y1 - thickness
-		puts "#{x1} #{y1} X #{x2} #{y2}"
-		 draw_box(x1, y1, x2, y2)
+
 
 		# Fuse them into the part
 		# Turn into group
