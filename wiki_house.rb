2993e209d156afe6c4f3a84f5cf96144f36c1946
Dirk Elmendorf <dirk@r26d.com>
2015-03-25 09:11:55 -0500

    Moved some of the files to make it easier to see how things are grouped

--- wiki_house.rb
+++ wiki_house.rb
@@ -5,7 +5,7 @@ module WikiHouse
   DEV_MODE = true
   LOG_ON = DEV_MODE
   CURRENT_PLATFORM = (Object::RUBY_PLATFORM =~ /mswin/i) ? :windows :
-        ((Object::RUBY_PLATFORM =~ /darwin/i) ? :mac : :other) unless defined? CURRENT_PLATFORM
+      ((Object::RUBY_PLATFORM =~ /darwin/i) ? :mac : :other) unless defined? CURRENT_PLATFORM
 
   def self.build_menus?
     @build_menus.nil? || @build_menus ? true : false
@@ -15,39 +15,44 @@ module WikiHouse
     UI.messagebox(msg)
     return false
   end
+
   def self.plugin_file(filename, sub_directories = [])
     unless sub_directories.is_a? Array
       sub_directories = [sub_directories]
     end
     path_list = [File.dirname(__FILE__), sub_directories, filename].flatten!
+    # puts File.join(path_list) if LOG_ON
     File.join(path_list)
   end
+
   def self.init
     #puts "Calling"
     keepers = [:DEV_MODE, :LOG_ON, :CURRENT_PLATFORM, :Tools, :Tool]
     konstants = self.constants
     keepers.each do |keep|
-      konstants.reject! {|k| k.to_s.match(keep.to_s)}
+      konstants.reject! { |k| k.to_s.match(keep.to_s) }
     end
 
-    #Object.send(:remove_const, :Foo)
+
     puts "Reloading Constants #{konstants}" if LOG_ON
-    konstants.each {|k| self.send(:remove_const, k.to_sym)}
-    Dir[ plugin_file("*.rb", "helpers")].each do |file|
+    konstants.each { |k| self.send(:remove_const, k.to_sym) }
+    Dir[plugin_file("*.rb", "helpers")].each do |file|
       #puts file if LOG_ON
       load file
     end
-    Dir[ plugin_file("*.rb", "overrides")].each do |file|
+    Dir[plugin_file("*.rb", "overrides")].each do |file|
       #puts file if LOG_ON
       load file
     end
-    #load plugin_file("joint.rb","models") #Other files depend on this
-    load plugin_file("tools.rb","models") #Other files depend on this
 
-    Dir[ plugin_file("*.rb", "models")].each do |file|
-    #  puts file if LOG_ON
+    load plugin_file("tools.rb", ["models", "tools"]) #Other files depend on this
+
+    Dir[plugin_file("*.rb", ["models", "**"])].each do |file|
+      #puts file if LOG_ON
       load file unless ["tools.rb"].include? File.basename(file)
     end
+
+
     Tools.init_all
     @build_menus = false
   end
