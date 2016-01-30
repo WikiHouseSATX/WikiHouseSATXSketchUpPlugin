ee0aaeff181f17d29b89ab646eb7672875da6bc1
Dirk Elmendorf <dirk@r26d.com>
2016-01-30 07:16:02 -0600

    Working on getting the marking and flattnein gworking

--- models/parts/rising_barn_14_14.rb
+++ models/parts/rising_barn_14_14.rb
@@ -1,6 +1,6 @@
 class WikiHouse::RisingBarn1414
   include WikiHouse::PartHelper
-  SIP_OFFSET = 3.0/32.0
+  SIP_OFFSET = 0.0938
 
 
   class BarnPart
@@ -34,6 +34,9 @@ class WikiHouse::RisingBarn1414
     def make_pockets!
       []
     end
+    def mark_face!
+      
+    end
 
     def draw!
 
@@ -44,6 +47,7 @@ class WikiHouse::RisingBarn1414
       pockets = make_pockets!
 
       face = Sk.add_face(lines)
+
       set_material(face)
 
       if pockets && pockets.length > 0
@@ -55,8 +59,9 @@ class WikiHouse::RisingBarn1414
 
 
       make_part_right_thickness(face)
-      Sk.add_face(lines)
+      mark_face!
 
+      Sk.add_face(lines)
       set_group(face.all_connected)
       mark_primary_face!(face)
     end
@@ -68,15 +73,17 @@ class WikiHouse::RisingBarn1414
   end
   class Spline < BarnPart
 
-      #this is just a part with a fixed width and a variable length
-    def initialize(length: nil, parent_part: nil,  group: nil, origin: nil, label: label)
+    #this is just a part with a fixed width and a variable length
+    def initialize(length: nil, parent_part: nil, group: nil, origin: nil, label: label)
       part_init(origin: origin, sheet: WikiHouse::Basswood332Sheet.new, label: label, parent_part: parent_part)
       @length = length
     end
+
     def length
       @length
 
     end
+
     def width
       WikiHouse::CoroplastSheet.new.thickness
     end
@@ -89,6 +96,10 @@ class WikiHouse::RisingBarn1414
 
     def width
       9.227
+    end
+
+    def spline_lengths
+      [4.487, width - 8.623]
     end
 
     def make_points
@@ -132,6 +143,10 @@ class WikiHouse::RisingBarn1414
 
     def width
       9.227
+    end
+
+    def spline_lengths
+      [width]
     end
 
     def make_points
@@ -195,6 +210,10 @@ class WikiHouse::RisingBarn1414
       9.227
     end
 
+    def spline_lengths
+      [width]
+    end
+
     def make_points
       t = thickness
       part_points = []
@@ -234,6 +253,10 @@ class WikiHouse::RisingBarn1414
 
     def width
       9.227
+    end
+
+    def spline_lengths
+      [0.601, width - 2.763]
     end
 
     def make_points
@@ -289,7 +312,9 @@ class WikiHouse::RisingBarn1414
     def width
       9.227
     end
-
+    def spline_lengths
+      [width, length, width]
+    end
 
     def make_points
       t = thickness
@@ -322,25 +347,54 @@ class WikiHouse::RisingBarn1414
       9.227
     end
 
+    def spline_lengths
+      [length, width, length, width]
+    end
+    def mark_face!
+      inside_points = []
+      puts bounding_c1.z, thickness, bounding_c1.z + thickness, bounding_c1.z + 2 * thickness
+      inside_points << [bounding_c1.x + SIP_OFFSET, bounding_c1.y + SIP_OFFSET, bounding_c1.z + 1 * thickness]
+      inside_points << [bounding_c1.x + SIP_OFFSET, bounding_c1.y + length - SIP_OFFSET, bounding_c1.z + 1 * thickness]
+      inside_points << [bounding_c1.x + width - SIP_OFFSET, bounding_c1.y + length - SIP_OFFSET, bounding_c1.z + 1 * thickness]
+      inside_points << [bounding_c1.x + width - SIP_OFFSET, bounding_c1.y + SIP_OFFSET, bounding_c1.z + 1 * thickness]
+
+      lines = Sk.draw_all_points(inside_points)
+    end
   end
   class FloorInside < FloorFace
     def length
-      super - SIP_OFFSET
+      super - SIP_OFFSET * 2
     end
 
     def width
-      super - SIP_OFFSET
+      super - SIP_OFFSET * 2
+
+    end
+    def mark_face!
 
     end
   end
 
   attr_reader :floor_face, :floor_inside,
-              :left_roof_face, :left_roof_inside,
-              :right_roof_face, :right_roof_inside,
+              :floor_splines,
+
               :back_wall_face, :back_wall_inside,
+              :back_wall_splines,
+
+              :front_wall_face, :front_wall_inside,
+              :front_wall_splines,
+
               :left_wall_face, :left_wall_inside,
+              :left_wall_splines,
+
               :right_wall_face, :right_wall_inside,
-              :front_wall_face, :front_wall_inside
+              :right_wall_bottom_spline,
+
+              :left_roof_face, :left_roof_inside,
+              :left_roof_splines,
+              
+              :right_roof_face, :right_roof_inside,
+              :right_roof_splines
 
 
   def initialize(origin: nil, sheet: nil, label: nil, parent_part: nil)
@@ -348,49 +402,68 @@ class WikiHouse::RisingBarn1414
 
     #Need to make
     # Splines
-  #Do splines need to be beveled?
-
-    # Bottom of Left Wall
-    # Bottom of Right Wall
-    # Bottom of Front Wall (two parts)
-    # Bottom of Left wall (two parts)
-    # Bottom of Back Wall
-    # Left Roof side , front & back
-    # Right Roof Side, front & back
+    #Do splines need to be beveled? -
+    #What about spline on outside of roof - is it wider?
 
     #Need to mark the offsets for the inside piece
-
+    #Need to handle flattening and exporting to svg - keeping the parts on the right material
 
 
     @floor_face = FloorFace.new(label: "FloorFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new,
                                 parent_part: self)
     @floor_inside = FloorInside.new(label: "FloorInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
 
-    @floor_top_spline = Spline.new(length: @floor_face.length,label: "FloorTopSpline", origin: @origin, parent_part: self)
-    @floor_bottom_spline = Spline.new(length: @floor_face.length,label: "FloorBottomSpline", origin: @origin, parent_part: self)
-
-    @floor_left_spline = Spline.new(length: @floor_face.width,label: "FloorLeftSpline", origin: @origin, parent_part: self)
-    @floor_right_spline = Spline.new(length: @floor_face.width,label: "FloorRightSpline", origin: @origin, parent_part: self)
+    @floor_splines = @floor_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "FloorSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
 
 
     @back_wall_face = BackWallFace.new(label: "BackWallFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @back_wall_inside = BackWallInside.new(label: "BackWallInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
 
+    @back_wall_splines = @back_wall_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "BackWallSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
+
+
     @front_wall_face = FrontWallFace.new(label: "FrontWallFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @front_wall_inside = FrontWallInside.new(label: "FrontWallInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
+
+
+    @front_wall_splines = @front_wall_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "FrontWallSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
+
 
     @left_wall_face = LeftWallFace.new(label: "LeftWallFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @left_wall_inside = LeftWallInside.new(label: "LeftWallInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
 
+    @left_wall_splines = @left_wall_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "LeftWallSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
+
+
     @right_wall_face = RightWallFace.new(label: "RightWallFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @right_wall_inside = RightWallInside.new(label: "RightWallInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
+
+    @right_wall_splines = @right_wall_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "RightWallSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
 
 
     @left_roof_face = RoofFace.new(label: "LeftRoofFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @left_roof_inside = RoofInside.new(label: "LeftRoofInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
 
+    @left_roof_splines = @left_roof_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "LeftRoofSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
+    
+    
     @right_roof_face = RoofFace.new(label: "RightRoofFace", origin: @origin, sheet: WikiHouse::Basswood332Sheet.new, parent_part: self)
     @right_roof_inside = RoofInside.new(label: "RightRoofInside", origin: @origin, sheet: WikiHouse::CoroplastSheet.new, parent_part: self)
+    @right_roof_splines = @right_roof_face.spline_lengths.collect.with_index do |spline_length, index|
+      Spline.new(length: spline_length, label: "RightRoofSpline-#{index + 1}", origin: @origin, parent_part: self)
+    end
 
 
   end
@@ -399,129 +472,87 @@ class WikiHouse::RisingBarn1414
     @origin = new_origin
     @floor_face.origin = @origin
     @floor_inside.origin = @origin
-
-    @floor_top_spline.origin = @origin
-    @floor_bottom_spline.origin = @origin
-    @floor_left_spline.origin = @origin
-    @floor_right_spline.origin = @origin
-
+    @floor_splines.each { |spline| spline.origin = @origin }
 
     @back_wall_face.origin = @origin
     @back_wall_inside.origin = @origin
+    @back_wall_splines.each { |spline| spline.origin = @origin }
 
     @front_wall_face.origin = @origin
     @front_wall_inside.origin = @origin
+    @front_wall_splines.each { |spline| spline.origin = @origin }
+
 
     @left_wall_face.origin = @origin
     @left_wall_inside.origin = @origin
+    @left_wall_splines.each { |spline| spline.origin = @origin }
 
     @right_wall_face.origin = @origin
     @right_wall_inside.origin = @origin
+    @right_wall_splines.each { |spline| spline.origin = @origin }
 
 
     @left_roof_face.origin = @origin
     @left_roof_inside.origin = @origin
+    @left_roof_splines.each { |spline| spline.origin = @origin }
 
+    
     @right_roof_face.origin = @origin
     @right_roof_inside.origin = @origin
+    @right_roof_splines.each { |spline| spline.origin = @origin }
+
   end
 
+
+  def draw_part!(face_part, inside_part, splines, placement_width)
+    face_part.draw!
+    face_part.move_by(x: placement_width).go!
+    placement_width += face_part.width + 5
+
+
+    inside_part.draw!
+    inside_part.move_by(x: placement_width).go!
+    placement_width += inside_part.width + 5
+
+    splines.each do |spline, index|
+      spline.draw!
+      spline.move_by(x: placement_width).go!
+      placement_width += spline.width + 5 + (index == 0 ? inside_part.width : 0)
+    end
+    placement_width
+
+  end
 
   def draw!
     Sk.find_or_create_layer(name: self.class.name)
     Sk.make_layer_active_name(name: self.class.name)
-    placement_width = @floor_face.width + 5
-    @floor_face.draw!
-    @floor_face.move_by(x: placement_width).go!
+    placement_width = 0
+    placement_width = draw_part!(@floor_face, @floor_inside, @floor_splines, placement_width)
+#    placement_width = draw_part!(@back_wall_face, @back_wall_inside, @back_wall_splines, placement_width)
+#    placement_width = draw_part!(@front_wall_face, @front_wall_inside, @front_wall_splines, placement_width)
+#    placement_width = draw_part!(@left_wall_face, @left_wall_inside, @left_wall_splines, placement_width)
+#    placement_width = draw_part!(@right_wall_face, @right_wall_inside, @right_wall_splines, placement_width)
 
+ #   placement_width = draw_part!(@left_roof_face, @left_roof_inside, @left_roof_splines, placement_width)
+ #   placement_width = draw_part!(@right_roof_face, @right_roof_inside, @right_roof_splines, placement_width)
 
-    @floor_inside.draw!
-    placement_width += @floor_inside.width + 5
-    @floor_inside.move_by(x: placement_width).go!
-
-
-    @floor_top_spline.draw!
-    placement_width += @floor_top_spline.width + 5 + @floor_inside.width
-    @floor_top_spline.move_by(x: placement_width).go!
-
-    @floor_bottom_spline.draw!
-    placement_width += @floor_bottom_spline.width + 5
-    @floor_bottom_spline.move_by(x: placement_width).go!
-
-    @floor_left_spline.draw!
-    placement_width += @floor_left_spline.width + 5
-    @floor_left_spline.move_by(x: placement_width).go!
-
-    @floor_right_spline.draw!
-    placement_width += @floor_right_spline.width + 5
-    @floor_right_spline.move_by(x: placement_width).go!
-
-    placement_width += @back_wall_face.width + 5
-    @back_wall_face.draw!
-    @back_wall_face.move_by(x: placement_width).go!
-
-
-    @back_wall_inside.draw!
-    placement_width += @back_wall_inside.width + 5
-    @back_wall_inside.move_by(x: placement_width).go!
-
-    placement_width += @front_wall_face.width + 5
-    @front_wall_face.draw!
-    @front_wall_face.move_by(x: placement_width).go!
-
-
-    @front_wall_inside.draw!
-    placement_width += @front_wall_inside.width + 5
-    @front_wall_inside.move_by(x: placement_width).go!
-
-    placement_width += @left_wall_face.width + 5
-    @left_wall_face.draw!
-    @left_wall_face.move_by(x: placement_width).go!
-
-
-    @left_wall_inside.draw!
-    placement_width += @left_wall_inside.width + 5
-    @left_wall_inside.move_by(x: placement_width).go!
-
-
-    placement_width += @right_wall_face.width + 5
-    @right_wall_face.draw!
-    @right_wall_face.move_by(x: placement_width).go!
-
-
-    @right_wall_inside.draw!
-    placement_width += @right_wall_inside.width + 5
-    @right_wall_inside.move_by(x: placement_width).go!
-
-
-    @left_roof_face.draw!
-    placement_width += @left_roof_face.width + 5
-    @left_roof_face.move_by(x: placement_width).go!
-
-    @left_roof_inside.draw!
-    placement_width += @left_roof_inside.width + 5
-    @left_roof_inside.move_by(x: placement_width).go!
-
-    @right_roof_face.draw!
-    placement_width += @right_roof_face.width + 5
-    @right_roof_face.move_by(x: placement_width).go!
-
-    @right_roof_inside.draw!
-    placement_width += @right_roof_inside.width + 5
-    @right_roof_inside.move_by(x: placement_width).go!
-
-
-    
-    
     groups = [@floor_face.group, @floor_inside.group,
-              @floor_top_spline.group, @floor_bottom_spline.group,
-              @floor_left_spline.group, @floor_right_spline.group,
+
               @back_wall_face.group, @back_wall_inside.group,
               @front_wall_face.group, @front_wall_inside.group,
               @left_wall_face.group, @left_wall_inside.group,
               @right_wall_face.group, @right_wall_inside.group,
               @left_roof_face.group, @left_roof_inside.group,
               @right_roof_face.group, @right_roof_inside.group]
+    @floor_splines.each { |spline| groups << spline.group }
+    @back_wall_splines.each { |spline| groups << spline.group }
+    @front_wall_splines.each { |spline| groups << spline.group }
+    @left_wall_splines.each { |spline| groups << spline.group }
+    @right_wall_splines.each { |spline| groups << spline.group }
+    @left_roof_splines.each { |spline| groups << spline.group }
+    @right_roof_splines.each { |spline| groups << spline.group }
+
+    
     set_group(groups.compact)
   end
 
